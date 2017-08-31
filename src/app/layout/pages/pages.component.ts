c638948f0facb080206aeef64c0ada329fa98f7b
Basant Kumar Sharma <scriptclump@gmail.com>
2017-08-31 14:39:26 +0530

    Angular-4 Bootstrap-4 Datatable in page module

--- src/app/layout/pages/pages.component.ts
+++ src/app/layout/pages/pages.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+
+import { DataTable, DataTableTranslations, DataTableResource } from 'angular-4-data-table-bootstrap-4';
+import { films } from './data-table-demo3-data';
 
 @Component({
     selector: 'app-pages',
@@ -8,9 +11,33 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class PagesComponent implements OnInit {
+    filmResource = new DataTableResource(films);
+    films = [];
+    filmCount = 0;
+
+    @ViewChild(DataTable) filmsTable;
+
     constructor() {
+        this.filmResource.count().then(count => this.filmCount = count);
     }
 
+    reloadFilms(params) {
+        this.filmResource.query(params).then(films => this.films = films);
+    }
+
+    cellColor(car) {
+        return 'rgb(255, 255,' + (155 + Math.floor(100 - ((car.rating - 8.7)/1.3)*100)) + ')';
+    };
+
+    // special params:
+
+    translations = <DataTableTranslations>{
+        indexColumn: 'Index column',
+        expandColumn: 'Expand column',
+        selectColumn: 'Select column',
+        paginationLimit: 'Max results',
+        paginationRange: 'Result range'
+    };
     ngOnInit() {
     }
 }
--- src/app/layout/pages/pages.module.ts
+++ src/app/layout/pages/pages.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Importing datatable
+import { DataTableModule } from 'angular-4-data-table-bootstrap-4';
+
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
+import { PageHeaderModule } from './../../shared';
 
 @NgModule({
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    PageHeaderModule,
+    DataTableModule
   ],
   declarations: [PagesComponent]
 })
