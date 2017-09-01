6aed5deb1460bccbdff13074721eb66b40470b6e
Basant Kumar Sharma <scriptclump@gmail.com>
2017-09-01 16:17:03 +0530

    Clean up: Removed All unneccessary files

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -17,14 +17,7 @@ const routes: Routes = [
             { path: 'faq/add', loadChildren: './faq-add/faq-add.module#FAQAddModule' },
             { path: 'contact-us', loadChildren: './contacts/contacts.module#ContactsModule' },
             { path: 'settings', loadChildren: './settings/settings.module#SettingsModule' },
-            { path: 'profile', loadChildren: './profile/profile.module#ProfileModule' },
-            { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
-            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' },
+            { path: 'profile', loadChildren: './profile/profile.module#ProfileModule' }
         ]
     }
 ];
